e79ac78a79addc71afb8082704ae6a2f5e7effac
varad583 <varadj583@gmail.com>
2026-01-04 21:28:45 +0530

    Add first Java main program

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Core Practice Repository");
+        System.out.println("Learning Core Java concepts step by step.");
+    }
+}
